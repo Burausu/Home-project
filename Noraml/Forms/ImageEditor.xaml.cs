644ce8cd44442217c96299fa7b11f03b7343c912
Burausu <panco74@mail.ru>
2020-06-06 02:14:18 +0300

    Add files via upload

---
+++ Noraml/Forms/ImageEditor.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Noraml
+{
+    /// <summary>
+    /// Interaction logic for ImageEditor.xaml
+    /// </summary>
+    public partial class ImageEditor : Window
+    {
+        public ImageEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void EditorWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = new ImageEditorViewModel(imageName.Text, editorWin);
+        }
+    }
+}
---
+++ Noraml/Forms/ImagesWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Linq;
+using WpfAnimatedGif;
+
+namespace Noraml
+{
+    /// <summary>
+    /// Interaction logic for ImagesWindow.xaml
+    /// </summary>
+    public partial class ImagesWindow : Window
+    {
+        public ImagesWindow()
+        {
+            InitializeComponent();
+            DataContext = new ImageViewModel();
+        }
+
+        /// <summary>
+        /// Перенаправляет на форму добавления тегов.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ImageEditor w = new ImageEditor();
+            ImageInfo element = listBox.SelectedItem as ImageInfo;
+            w.imageName.Text = element.NameInDataBase;
+            using (DataBase db = new DataBase())
+            {
+                if (db.Images.Where(s => s.ImageName == element.NameInDataBase).Single().ImageExstension == "gif")
+                {
+                    ImageBehavior.SetAnimatedSource(w.currentImage, element.Source);
+                }
+                else
+                {
+                    w.currentImage.Source = element.Source;
+                }
+            }
+            w.ShowDialog();
+        }
+
+        /// <summary>
+        /// Запрещает писать в <see cref="System.Windows.Controls.TextBox"/> что-то, кроме цифр.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PageBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!char.IsDigit((char)KeyInterop.VirtualKeyFromKey(e.Key)) & (e.Key != Key.Back | e.Key == Key.Space) & (e.Key != Key.Enter | e.Key != Key.Return))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
